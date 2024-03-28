ef59fe930589fef8a14a54b5bc8b31617f853000
Eduardo <edusoamar@gmail.com>
2024-03-27 21:38:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //trocar imagem
+  //se tiver light mode,oculos escuro
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Mano de óculos escuro pra não ficar cegueta")
+  } else {
+    //se não tiver light mode,manter a atual
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Junin macumba esplendido")
+  }
+}
